234e8894a4154ba07993f4b0c970986132b62e5d
Frans Paasonen <franspaasonen@gmail.com>
2020-05-10 22:57:37 +0300

    Return main.rs to original state for merging/testing of features

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-mod application;
-use application::game::Game;
-
 fn main() {
-    let mut game = Game::new();
-    game.add_ticker(1);
-
-    game.start_ticker(& || {
-        println!("tick");
-    });
+    println!("Hello, world!");
 }
