136871fc7c08b0d0e53786ebcc6b435a8457d2bc
Frans Paasonen <franspaasonen@gmail.com>
2022-02-21 02:12:47 +0200

    feat: Add builder for application

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,28 +1,70 @@
 pub mod input;
 pub mod loader;
 
-use crate::{engine::input::Input, renderer::Renderer, Camera2D, Time};
 use std::time::Instant;
+
+use self::loader::Loader;
+use crate::{engine::input::Input, renderer::Renderer, Camera2D, Time};
+use legion::{systems::ParallelRunnable, Resources, Schedule, World};
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
 };
 
-use self::loader::Loader;
+pub struct AppBuilder {
+    schedule_builder: legion::systems::Builder,
+}
+
+impl Default for AppBuilder {
+    fn default() -> AppBuilder {
+        AppBuilder {
+            schedule_builder: Schedule::builder(),
+        }
+    }
+}
+
+impl AppBuilder {
+    pub fn with_systems<T: ParallelRunnable + 'static>(mut self, systems: Vec<T>) -> Self {
+        for system in systems {
+            self.schedule_builder.add_system(system);
+        }
+        self
+    }
+
+    pub fn with_system<T: ParallelRunnable + 'static>(mut self, system: T) -> Self {
+        self.schedule_builder.add_system(system);
+        self
+    }
+
+    pub fn build(mut self) -> Application {
+        Application::new(self.schedule_builder.build())
+    }
+}
 
 pub struct Application {
-    event_loop: EventLoop<()>,
     window: Window,
+    event_loop: EventLoop<()>,
     default_camera: Camera2D,
+
+    renderer: Renderer,
+
+    schedule: Schedule,
+
     start_time: Instant,
     last_render_time: Instant,
-    renderer: Renderer,
+
+    pub world: World,
     pub loader: Loader,
+    pub resources: Resources,
 }
 
 impl Application {
-    pub fn new() -> Self {
+    pub fn builder() -> AppBuilder {
+        AppBuilder::default()
+    }
+
+    pub(crate) fn new(schedule: Schedule) -> Self {
         let event_loop = EventLoop::new();
         let window = WindowBuilder::new().build(&event_loop).unwrap();
 
@@ -33,10 +75,16 @@ impl Application {
         let start_time = Instant::now();
         let last_render_time = start_time;
 
+        let world = World::default();
+        let resources = Resources::default();
+
         Self {
+            world,
             window,
-            renderer,
             loader,
+            renderer,
+            schedule,
+            resources,
             start_time,
             event_loop,
             default_camera,
@@ -44,30 +92,25 @@ impl Application {
         }
     }
 
-    pub fn run<F: 'static>(
-        mut self,
-        mut world: legion::World,
-        mut resources: legion::Resources,
-        mut run_on_update: F,
-    ) where
-        F: FnMut(&mut legion::World, &mut legion::Resources) -> (),
-    {
+    pub fn run(mut self) {
         println!("Starting application");
 
         let time = Time::default();
         let input = Input::default();
 
-        resources.insert(time);
-        resources.insert(input);
+        self.resources.insert(time);
+        self.resources.insert(input);
 
         self.event_loop.run(move |event, _, control_flow| {
             *control_flow = ControlFlow::Poll;
             match event {
                 Event::RedrawRequested(window_id) if window_id == self.window.id() => {
-                    run_on_update(&mut world, &mut resources);
+                    {
+                        self.schedule.execute(&mut self.world, &mut self.resources);
+                    }
 
                     match self.renderer.render(
-                        &world,
+                        &self.world,
                         &mut self.loader,
                         &self.default_camera.bind_group,
                     ) {
@@ -84,14 +127,14 @@ impl Application {
                     self.last_render_time = now;
 
                     {
-                        let mut time = resources.get_mut::<Time>().unwrap();
+                        let mut time = self.resources.get_mut::<Time>().unwrap();
                         time.elapsed = elapsed;
                         time.delta_time = dt;
                     }
 
                     // Reset mouse delta after current update loop
                     {
-                        let mut input = resources.get_mut::<Input>().unwrap();
+                        let mut input = self.resources.get_mut::<Input>().unwrap();
                         input.reset_mouse_delta();
                     }
                 }
@@ -99,7 +142,7 @@ impl Application {
                     self.window.request_redraw();
                 }
                 Event::DeviceEvent { ref event, .. } => {
-                    let mut input = resources.get_mut::<Input>().unwrap();
+                    let mut input = self.resources.get_mut::<Input>().unwrap();
                     input.process_device_event(event);
                 }
                 Event::WindowEvent {
@@ -107,7 +150,7 @@ impl Application {
                     window_id,
                 } => {
                     if window_id == self.window.id() {
-                        let mut input = resources.get_mut::<Input>().unwrap();
+                        let mut input = self.resources.get_mut::<Input>().unwrap();
                         match event {
                             WindowEvent::ModifiersChanged(modifiers_state) => {
                                 input.update_modifiers_state(*modifiers_state)
